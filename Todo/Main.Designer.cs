d88e1320f15e4a60934546babd95d488800be04a
Jongseung Kim <jngsngkm@gmail.com>
2022-07-18 18:27:24 +0900

    구조 및 내용 적용

--- Todo/Main.Designer.cs
+++ Todo/Main.Designer.cs
@@ -28,49 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.PN_Date = new System.Windows.Forms.Panel();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.PN_T1 = new System.Windows.Forms.Panel();
+            this.Title1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.CB_Date = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PN_Date.SuspendLayout();
+            this.PN_T1.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // PN_Date
+            // 
+            this.PN_Date.Controls.Add(this.dateTimePicker1);
+            this.PN_Date.Location = new System.Drawing.Point(2, 2);
+            this.PN_Date.Name = "PN_Date";
+            this.PN_Date.Size = new System.Drawing.Size(580, 35);
+            this.PN_Date.TabIndex = 0;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker1.Font = new System.Drawing.Font("서울남산 장체 M", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker1.Location = new System.Drawing.Point(190, 5);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(222, 26);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // PN_T1
+            // 
+            this.PN_T1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PN_T1.Controls.Add(this.Title1);
+            this.PN_T1.Location = new System.Drawing.Point(2, 40);
+            this.PN_T1.Name = "PN_T1";
+            this.PN_T1.Size = new System.Drawing.Size(580, 150);
+            this.PN_T1.TabIndex = 1;
+            // 
+            // Title1
+            // 
+            this.Title1.AutoSize = true;
+            this.Title1.Font = new System.Drawing.Font("서울한강 장체 M", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Title1.Location = new System.Drawing.Point(3, 3);
+            this.Title1.Name = "Title1";
+            this.Title1.Size = new System.Drawing.Size(56, 15);
+            this.Title1.TabIndex = 0;
+            this.Title1.Text = "오늘 할 일";
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.CB_Date);
-            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(2, 196);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(584, 30);
-            this.panel1.TabIndex = 0;
+            this.panel1.Size = new System.Drawing.Size(580, 150);
+            this.panel1.TabIndex = 2;
             // 
-            // CB_Date
+            // label1
             // 
-            this.CB_Date.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.CB_Date.Font = new System.Drawing.Font("휴먼둥근헤드라인", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.CB_Date.FormattingEnabled = true;
-            this.CB_Date.Items.AddRange(new object[] {
-            "22.07.18",
-            "22.07.19"});
-            this.CB_Date.Location = new System.Drawing.Point(0, 0);
-            this.CB_Date.Name = "CB_Date";
-            this.CB_Date.Size = new System.Drawing.Size(584, 33);
-            this.CB_Date.TabIndex = 1;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("서울한강 장체 M", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(3, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "할 일 리스트";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(2, 352);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(580, 150);
+            this.panel2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("서울한강 장체 M", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(3, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "서류 리스트";
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(2, 508);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(580, 150);
+            this.panel3.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("서울한강 장체 M", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(3, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "구매 리스트";
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 611);
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(584, 661);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.PN_T1);
+            this.Controls.Add(this.PN_Date);
             this.Name = "Main";
             this.Text = " Todo";
             this.Load += new System.EventHandler(this.Main_Load);
+            this.PN_Date.ResumeLayout(false);
+            this.PN_T1.ResumeLayout(false);
+            this.PN_T1.PerformLayout();
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private Panel PN_Date;
+        private DateTimePicker dateTimePicker1;
+        private Panel PN_T1;
+        private Label Title1;
         private Panel panel1;
-        private ComboBox CB_Date;
+        private Label label1;
+        private Panel panel2;
+        private Label label2;
+        private Panel panel3;
+        private Label label3;
     }
 }
