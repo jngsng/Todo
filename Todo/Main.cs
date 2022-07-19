198217b1d5e9b4e6d008e9ec015d0fc515a7d181
Jongseung Kim <jngsngkm@gmail.com>
2022-07-19 18:30:43 +0900

    CSV 파일 불러오기 (ing)

--- Todo/Main.cs
+++ Todo/Main.cs
@@ -5,6 +5,7 @@ namespace Todo
     public partial class Main : Form
     {
         string today = "";
+        List<string> csvList = null;
 
         public Main()
         {
@@ -26,15 +27,31 @@ namespace Todo
 
         private void button1_Click(object sender, EventArgs e)
         {
-
-
             string dir = Environment.CurrentDirectory + @"\date";
             
+            // 폴더 생성
             DirectoryInfo di = new DirectoryInfo(dir);
             if(!di.Exists) Directory.CreateDirectory(dir);
 
             dir += @"\" + DateTime.Now.ToString("Y");
             if (!di.Exists) Directory.CreateDirectory(dir);
+
+            // CSV 파일 가져오기
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if(fbd.ShowDialog() == DialogResult.OK)
+            {
+                string[] filename = Directory.GetFiles(fbd.SelectedPath);
+                csvList = filename.Where(x => x.IndexOf('.csv',StringComparison.OrdinalIgnoreCase >= 0).Select(x => x).ToList());
+
+                string result = string.Empty;
+
+                try
+                {
+                    GetCSVData(csvList, result);
+                }
+                catch { }
+            }
 
             //string dir = Environment.CurrentDirectory;
             label1.Text = dir;
@@ -45,6 +62,11 @@ namespace Todo
 
             //dataGridView1.DataSource = ds.Tables[0];
 
+        }
+
+        public void GetCSVData(List<string> csvList, string result)
+        {
+           
         }
     }
 }
